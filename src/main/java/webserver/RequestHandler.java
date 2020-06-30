69ed331c19c73f9d3d8a359867c0996e9eabc09b
Gyumin Kim <starkying@gmail.com>
2020-07-01 00:03:10 +0900

    3장 요구사항 구현
    * 요구사항 6: 사용자 목록 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,17 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-import java.nio.file.Files;
-import java.util.Map;
-
 import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Collection;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -36,12 +37,17 @@ public class RequestHandler extends Thread {
             String url = RequestLineParser.extractUrl(line);
             String requestPath = RequestLineParser.extractRequestPath(url);
             int contentLength = 0;
+            String cookies = "";
             while (!line.equals("")) {
                 line = bufferedReader.readLine();
                 log.debug("line: {}", line);
                 HttpRequestUtils.Pair header = HttpRequestUtils.parseHeader(line);
-                if (header != null && header.getKey().equals("Content-Length")) {
-                    contentLength = Integer.parseInt(header.getValue());
+                if (header != null) {
+                    if (header.getKey().equals("Content-Length")) {
+                        contentLength = Integer.parseInt(header.getValue());
+                    } else if (header.getKey().equals("Cookie")) {
+                        cookies = header.getValue();
+                    }
                 }
             }
             DataOutputStream dos = new DataOutputStream(out);
@@ -53,12 +59,10 @@ public class RequestHandler extends Thread {
                 String name = parameters.get("name");
                 String email = parameters.get("email");
                 User user = new User(userId, password, name, email);
-                log.debug("User 객체 생성: {}", user);
                 DataBase.addUser(user);
                 byte[] body = getBody("/");
                 response302Header(dos, "/");
                 responseBody(dos, body);
-                return;
             } else if (requestPath.equals("/user/login")) {
                 String data = IOUtils.readData(bufferedReader, contentLength);
                 Map<String, String> parameters = HttpRequestUtils.parseQueryString(data);
@@ -67,7 +71,7 @@ public class RequestHandler extends Thread {
                 User userById = DataBase.findUserById(userId);
                 String path = "";
                 if (userById.getPassword().equals(password)) {
-                    path =  "/";
+                    path = "/";
                     byte[] body = getBody(path);
                     response200WithLoginHeader(dos, true);
                     responseBody(dos, body);
@@ -77,11 +81,36 @@ public class RequestHandler extends Thread {
                     response200WithLoginHeader(dos, false);
                     responseBody(dos, body);
                 }
-                return;
+            } else if (requestPath.equals("/user/list")) {
+                Map<String, String> cookie = HttpRequestUtils.parseCookies(cookies);
+                boolean isLogined = Boolean.parseBoolean(cookie.get("logined"));
+                if (isLogined) {
+                    Collection<User> users = DataBase.findAll();
+                    long id = 1;
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("<table border='1'>");
+                    for (User user : users) {
+                        sb.append("<sb>");
+                        sb.append("<th scope=\"row\">").append(id++).append("</th>");
+                        sb.append("<td>").append(user.getUserId()).append("</td>");
+                        sb.append("<td>").append(user.getName()).append("</td>");
+                        sb.append("<td>").append(user.getEmail()).append("</td>");
+                        sb.append("</sb>");
+                    }
+                    sb.append("</table>");
+                    byte[] body = sb.toString().getBytes();
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                } else {
+                    byte[] body = getBody("/user/login.html");
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                }
+            } else {
+                byte[] body = getBody(requestPath);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             }
-            byte[] body = getBody(requestPath);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
